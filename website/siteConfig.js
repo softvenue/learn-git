14ed7f591ddc10ec74b2560a9ccd7f2a2ffdb077
celyes <celyes02@gmail.com>
2019-03-09 01:42:11 +0100

    update file - siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Learn GIT', // Title for your website.
   tagline: 'A website for learning git',
   url: 'https://learn-git.ga', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/learn-git/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
